36438e84c5e7ae7bcd0a5e5759349f98ce04d5dc
sh <shenkang2@gome.com.cn>
2022-11-03 01:21:44 +0800

    design

---
+++ src/main/java/com/opencare/sort/test/CronTest.java
@@ -0,0 +1,13 @@
+package com.opencare.sort.test;
+
+import java.text.SimpleDateFormat;
+
+public class CronTest {
+    public static void main(String[] args) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        //CronExpression cronExpression = new CronExpression("0 0/5 8-19 * * ? *");
+        //
+        //boolean resCron = cronExpression.isSatisfiedBy(simpleDateFormat.parse("2019-09-09 19:55:00"));
+    }
+}
